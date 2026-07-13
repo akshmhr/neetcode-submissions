2912781ccca3d1a8e050ccea796c6a9337cdc94e
Aksh Agrawal <anshuagrawal123456@gmail.com>
2026-07-14 02:45:56 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, piles, h, speed):
+        hours = 0
+        for pile in piles:
+            hours += (pile + speed - 1) // speed
+        return hours<=h
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+
+        while left < right:
+            mid = (left+right) // 2
+            if self.canFinish(piles, h, mid):
+                right = mid
+            else:
+                left = mid+1
+
+
+        return left
+    
+
+
+

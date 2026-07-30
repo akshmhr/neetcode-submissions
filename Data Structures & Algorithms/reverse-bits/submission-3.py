c6b1e034fc786c40fe61bffbb4e7b5384020051a
Aksh Agrawal <anshuagrawal123456@gmail.com>
2026-07-31 01:47:24 +0530

    Add: reverse-bits - submission-3

---
+++ Data Structures & Algorithms/reverse-bits/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        rev = 0
+        for i in range(32):
+            rev <<= 1
+            rev |= (n & 1)
+            n >>= 1
+
+        return rev
+            

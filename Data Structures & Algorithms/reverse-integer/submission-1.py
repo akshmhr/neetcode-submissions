d74e0f916adba8d0a3a899f0f13c2cfecd9672b5
Aksh Agrawal <anshuagrawal123456@gmail.com>
2026-08-01 00:08:31 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        ans = 0
+
+        while x:
+            digit = x % 10
+            x //= 10
+
+            if ans > 214748364 or (ans == 214748364 and digit > 7):
+                return 0
+
+            ans = ans * 10 + digit
+
+        ans *= sign
+
+        return ans

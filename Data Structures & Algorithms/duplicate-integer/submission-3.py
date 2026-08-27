81426990a50bcc56a35ab37caab0883614d9dda1
Aksh Agrawal <anshuagrawal123456@gmail.com>
2026-08-28 01:24:51 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash_set = set(nums)
+        if len(hash_set) != len(nums):
+            return True
+        
+        return False
+            
